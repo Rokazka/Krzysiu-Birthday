d81752105f09e6616a422f18766acbc76b35e191
Kasia Napieralska <k.napieralska@gmail.com>
2023-09-23 17:08:36 +0100

    update

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ function openLtter() {
     letter.classList.toggle('active');
     circle.classList.toggle('heart-active');
     if (circle.classList.contains('heart-active')) {
+
         h1.classList.remove('changeTwo')
         h1.classList.add('change')
         envelope.classList.remove("close");
@@ -19,6 +20,7 @@ function openLtter() {
         h1.classList.remove('change')
         h1.classList.add('changeTwo')
         envelope.classList.remove("open");
+
         envelope.classList.add("close");
     }
 
